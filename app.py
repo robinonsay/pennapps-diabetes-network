f755f445ee101c029486a90ddad306bedba67979
robinonsay <raomax@gmail.com>
2016-01-23 06:55:24 -0500

    Working test code, but does not display logo

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+app = Flask(__name__)
 
-def main():
-    return render_template('index.html')
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(host='173.255.234.84')
