72a922820a1da35f872324925f661e8e4079a37f
robinonsay <raomax@gmail.com>
2016-01-23 09:53:44 -0500

    Wrote login() to receive post

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods = ['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    print(username + " : "+password)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.debug = True
