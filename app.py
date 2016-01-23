f2dfe4572104e04ca42837c51a8e19cdfb11b28b
robinonsay <raomax@gmail.com>
2016-01-23 07:39:19 -0500

    Added a route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/<imagelogo>')
 def index(imagelogo=None):
     imagelogo = url_for('static', filename='penapps-diabetes-logo-jumbo.png')
     return render_template('index.html',imagelogo=imagelogo)
