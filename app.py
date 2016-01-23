b592b825d410e103c3855e96a9988b6c361a7b62
robinonsay <raomax@gmail.com>
2016-01-23 07:30:29 -0500

    Added debug flag

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(imagelogo=None):
-  imagelogo = url_for('static', filename='penapps-diabetes-logo-jumbo.png')
-  return render_template('index.html',imagelogo=imagelogo)
+    imagelogo = url_for('static', filename='penapps-diabetes-logo-jumbo.png')
+    return render_template('index.html',imagelogo=imagelogo)
 
 if __name__ == '__main__':
-  app.run(host='173.255.234.84')
+    app.debug = True
+    app.run(host='173.255.234.84')
