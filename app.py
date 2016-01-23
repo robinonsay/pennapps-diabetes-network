e5fc9cc48a77d4345fbce020d8d15930a8d15633
robinonsay <raomax@gmail.com>
2016-01-23 07:25:18 -0500

    add code to get images

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def index(imagelogo=None):
+  imagelogo = url_for('static', filename='penapps-diabetes-logo-jumbo.png')
+  return render_template('index.html',imagelogo=imagelogo)
 
 if __name__ == '__main__':
   app.run(host='173.255.234.84')
