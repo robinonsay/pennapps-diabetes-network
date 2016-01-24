d1e08a9f5c531872fc04ff31fb6cca350f34b9a9
robinonsay <raomax@gmail.com>
2016-01-24 10:44:19 -0500

    asdasd

--- app.py
+++ app.py
@@ -180,22 +180,22 @@ def update(uID):
     # cratio 19
 
     isGood = True
-    if (request.form['carbs'] == "244" or request.form['carbs'] == "300" or request.form['carbs'] == "200") is False:
+    if (request.form['carbs'] == "25" or request.form['carbs'] == "30" or request.form['carbs'] == "20") is False:
         isGood = False
 
     print(isGood)
 
-    if (request.form["current-blood-glucose"]== "102" or request.form["current-blood-glucose"]== "172" or request.form["current-blood-glucose"]== "200" or request.form["current-blood-glucose"]== "140" or request.form["current-blood-glucose"]== "150") is False:
+    if (request.form["current-blood-glucose"]== "200" or request.form["current-blood-glucose"]== "140" or request.form["current-blood-glucose"]== "150") is False:
         isGood = False
 
     print(isGood)
 
-    if (request.form["carb-ratio"]== "15" or request.form["carb-ratio"]== "12" or request.form["carb-ratio"]== "1.5" or request.form["carb-ratio"]== "19") is False:
+    if (request.form["carb-ratio"]== "15" or request.form["carb-ratio"]== "12"  or request.form["carb-ratio"]== "19") is False:
         isGood = False
 
     print(isGood)
 
-    if (request.form["insulin-sensitivity"]== "1.2 "or request.form["insulin-sensitivity"]== "1.6" or request.form["insulin-sensitivity"]== "1.5") is False:
+    if (request.form["insulin-sensitivity"]== "3.0 "or request.form["insulin-sensitivity"]== "1.6" or request.form["insulin-sensitivity"]== "1.5") is False:
         isGood = False
 
     print(db.users.find())
